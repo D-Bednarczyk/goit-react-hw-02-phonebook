9477d6be96667eb9348aca518dec762aee5491f1
D-Bednarczyk <117660494+D-Bednarczyk@users.noreply.github.com>
2023-06-16 19:10:33 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,7 @@
 import React, { Component } from 'react';
 import css from './app.module.css';
 import { nanoid } from 'nanoid';
-
-const Form = props => {
-  return (
-    <form onSubmit={props.submitFunc}>
-      <label>Name</label>
-      <input
-        type="text"
-        name="name"
-        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        required
-      />
-      <label>Phone</label>
-      <input
-        type="tel"
-        name="number"
-        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-        required
-      />
-      <button>Add contacts</button>
-    </form>
-  );
-};
+import { Form } from './Form';
 
 const Filter = props => {
   return (
---
+++ src/components/Form.jsx
@@ -0,0 +1,23 @@
+export const Form = props => {
+  return (
+    <form onSubmit={props.submitFunc}>
+      <label>Name</label>
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+      <label>Phone</label>
+      <input
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+      <button>Add contacts</button>
+    </form>
+  );
+};
